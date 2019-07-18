d3e0d5361e2f4000d51ef781fe8c6e4c8d8380ff
Jacksard <jacobgoskha@gmail.com>
2019-07-17 20:46:06 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const connectDB = require('./config/db');
+
+// Connect to MongoDB
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server is running on port : ${PORT}`));
+
